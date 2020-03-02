9dcfcb3036169a026d65d66ad291195bc4278583
Ruchini Sujanthi <ruchini.sujanthi@arimaclanka.com>
2020-03-02 21:16:00 +0530

    PM tool initial development

---
+++ components/LayoutBase.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+    <v-list-item two-line :class="miniVariant && 'px-0'">
+        <v-avatar color="indigo">
+          <v-icon dark>mdi-account-circle</v-icon>
+        </v-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Arimac
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Ruchini Sujanthi
+          </v-list-item-subtitle>
+        </v-list-item-content>
+    </v-list-item>
+  
+    <v-divider></v-divider>
+      <v-list dense>
+        <v-list-item v-for="item in items"
+            :key="item.title"
+            link>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: true,
+        items: [
+          { title: 'Summary', icon: 'mdi-book-open' },
+          { title: 'Projects', icon: 'mdi-laptop-mac' },
+          { title: 'Tasks', icon: 'mdi-calendar-text' },
+          { title: 'Attendance', icon: 'mdi-calendar-check' },
+          { title: 'Inventory', icon: 'mdi-cart' },
+          { title: 'Admin', icon: 'mdi-account-circle' }
+        ],
+        color: 'primary',
+        colors: [
+          'primary',
+          'blue',
+          'success',
+          'red',
+          'teal'
+        ],
+        right: true,
+        miniVariant: false,
+        expandOnHover: false,
+        background: false
+      }
+    },
+    computed: {
+      bg () {
+        return this.background ? 'https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg' : undefined
+      }
+    }
+  }
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,133 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+    <v-list-item two-line :class="miniVariant && 'px-0'">
+        <v-avatar color="indigo">
+          <v-icon dark>mdi-account-circle</v-icon>
+        </v-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Arimac
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Ruchini Sujanthi
+          </v-list-item-subtitle>
+        </v-list-item-content>
+    </v-list-item>
+  
+    <v-divider></v-divider>
+      <v-list>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app
+      color="indigo"
+      dark fixed app :clipped-left="clipped">
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>web</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="fixed = !fixed"
+      >
+        <v-icon>remove</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        
+      </v-container>
+    </v-content>
+    <v-navigation-drawer
+      temporary
+      :right="right"
+      v-model="rightDrawer"
+      fixed
+    >
+      <v-list>
+        <v-list-tile @click.native="right = !right">
+          <v-list-tile-action>
+            <v-icon light>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: true,
+        items: [
+          { title: 'Summary', icon: 'mdi-book-open' },
+          { title: 'Projects', icon: 'mdi-laptop-mac' },
+          { title: 'Tasks', icon: 'mdi-calendar-text' },
+          { title: 'Attendance', icon: 'mdi-calendar-check' },
+          { title: 'Inventory', icon: 'mdi-cart' },
+          { title: 'Admin', icon: 'mdi-account-circle' }
+        ],
+        color: 'primary',
+        colors: [
+          'primary',
+          'blue',
+          'success',
+          'red',
+          'teal'
+        ],
+        right: true,
+        miniVariant: false,
+        expandOnHover: false,
+        background: false,
+        fixed: false,
+        rightDrawer: false,
+        clipped: false,
+        title: false
+
+      }
+    },
+    computed: {
+      bg () {
+        return this.background ? 'https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg' : undefined
+      }
+    }
+  }
+</script>
