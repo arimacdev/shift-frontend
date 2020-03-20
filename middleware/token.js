e3f824388a5e5f1635ce7162f5dee26fc24500a9
Naveen <naveenperera777@gmail.com>
2020-03-20 22:16:55 +0530

    Fix slice issue

--- middleware/token.js
+++ middleware/token.js
@@ -2,9 +2,9 @@
 var jwt = require('jsonwebtoken');
 
 export default async function ({ store, error , app}) {
-    let token = app.$auth.getToken('keycloak')
+    let token = " " + app.$auth.getToken('keycloak')
     store.commit('user/setAccessToken', token);
-    token = token.slice(7, token.length).trimLeft();
+    token = token.slice(8, token.length).trimLeft();
     const decodedJwt = jwt.decode(token); 
     store.commit('user/setUserId', decodedJwt);
   }
