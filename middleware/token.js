73112539c5c1a67309ff35255330d0910da2593b
Buddhika Priyabhashana <priyabhashana@arimaclanka.com>
2020-03-21 04:03:18 +0000

    Update token.js

--- middleware/token.js
+++ middleware/token.js
@@ -1,12 +1,15 @@
 // import jsonwebtoken from "jsonwebtoken";
 var jwt = require('jsonwebtoken');
-
 export default async function ({ store, error , app}) {
     let token = " " + app.$auth.getToken('keycloak')
+    console.log("access_token", token);
     store.commit('user/setAccessToken', token);
     token = token.slice(8, token.length).trimLeft();
     const decodedJwt = jwt.decode(token); 
     console.log("decodedJWT",decodedJwt);
-    store.commit('user/setUserId', decodedJwt.userId);
+    if(decodedJwt != null){
+      console.log("no auth token")
+      store.commit('user/setUserId', decodedJwt.userId);
+      // $auth.loginWith('keycloak')
+    }
   }
-   
