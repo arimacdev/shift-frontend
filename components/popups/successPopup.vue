ed19f31763fa436118e89c4a087e434adc2cd75f
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-03-22 15:36:57 +0530

    popup added for user/ project create

---
+++ components/popups/successPopup.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="popupDiv">
+    <transition name="slide-fade">
+    <v-card class="popupContent">
+        <v-list-item>
+        <v-list-item-action>
+                        <v-icon size="20" color="#2EC973">mdi-check</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content class="buttonText">
+                        <v-list-item-title class="popupTitle">Success</v-list-item-title>
+                        <v-list-item-subtitle class="popupSubtitle"> Successfully completed </v-list-item-subtitle>
+                    </v-list-item-content>
+     </v-list-item>
+    </v-card>
+    </transition>
+</div>
+</template>
+
+<style scoped>
+.slide-fade-enter-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+</style>
