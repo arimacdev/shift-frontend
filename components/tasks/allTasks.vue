c02b4adf4e4c5baf27ec9007f758d6e0fc2a363e
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-04-29 20:08:06 +0530

    project page updated

--- components/tasks/allTasks.vue
+++ components/tasks/allTasks.vue
@@ -106,12 +106,12 @@
         <div class="taskList restructuredMainTaskList">
           <v-list-item
             v-if="
-            task.taskStatus == taskSelect ||
-              taskFilter == 'none' ||
-              taskFilter == 'assignee' ||
-              taskFilter == 'dateRange' ||
-              taskSelect == 'all'
-          "
+              task.taskStatus == taskSelect ||
+                taskFilter == 'none' ||
+                taskFilter == 'assignee' ||
+                taskFilter == 'dateRange' ||
+                taskSelect == 'all'
+            "
             @click.stop="drawer = !drawer"
             @click="selectTask(task)"
           >
@@ -120,8 +120,11 @@
                 v-if="task.parentTask.taskStatus == 'closed'"
                 size="30"
                 color="#2EC973"
-              >mdi-checkbox-marked-circle</v-icon>
-              <v-icon v-else size="30" color="#FFFFFF">mdi-checkbox-blank-circle</v-icon>
+                >mdi-checkbox-marked-circle</v-icon
+              >
+              <v-icon v-else size="30" color="#FFFFFF"
+                >mdi-checkbox-blank-circle</v-icon
+              >
             </v-list-item-action>
             <div class="tasklistTaskNames restructuredMainTaskName">
               <div class="body-2">
@@ -129,12 +132,12 @@
                 {{ task.parentTask.taskName }}
               </div>
             </div>
-            <div class="restStatusChip" :class="statusCheck(task)">{{task.parentTask.issueType}}</div>
+            <div class="restStatusChip" :class="statusCheck(task)">
+              {{ task.parentTask.issueType }}
+            </div>
             <v-list-item-content class="updatedDate">
               <v-list-item-title :class="dueDateCheck(task)">
-                {{
-                getProjectDates(task.parentTask.taskDueDateAt)
-                }}
+                {{ getProjectDates(task.parentTask.taskDueDateAt) }}
               </v-list-item-title>
             </v-list-item-content>
             <v-list-item-avatar>
@@ -149,7 +152,12 @@
             </v-list-item-avatar>
             <a
               style="text-decoration: none;"
-              :href="'http://localhost:3000/task/'+ task.parentTask.taskId + '/?project=' + projectId"
+              :href="
+                'http://localhost:3000/task/' +
+                  task.parentTask.taskId +
+                  '/?project=' +
+                  projectId
+              "
               target="_blank"
             >
               <v-icon color="red">mdi-checkbox-blank-circle</v-icon>
@@ -170,41 +178,53 @@
             @keyup.enter="addParentTask"
           ></v-text-field>
         </div>
-        <div v-if="task.childTasks.length !== 0" class="taskList restructuredSubTaskList">
-          <v-list-item @click.stop="drawer = !drawer" @click="selectTask(task)" v-for="(childTask, index) in task.childTasks" :key="index">
-            <v-list-item-action>
-              <v-icon
-                v-if="task.taskStatus == 'closed'"
-                size="30"
-                color="#2EC973"
-              >mdi-checkbox-marked-circle</v-icon>
-              <v-icon v-else size="30" color="#FFFFFF">mdi-checkbox-blank-circle</v-icon>
-            </v-list-item-action>
-            <div class="tasklistTaskNames restructuredSubTaskName">
-              <div class="body-2">
-                <span class="restructuredMainTaskCode">MRI - #1</span>
-                {{ childTask.taskName }}
+        <div v-if="task.childTasks.length !== 0">
+          <div
+            v-for="(childTask, index) in task.childTasks"
+            :key="index"
+            class="taskList restructuredSubTaskList"
+          >
+            <v-list-item
+              @click.stop="drawer = !drawer"
+              @click="selectTask(task)"
+            >
+              <v-list-item-action>
+                <v-icon
+                  v-if="childTask.taskStatus == 'closed'"
+                  size="30"
+                  color="#2EC973"
+                  >mdi-checkbox-marked-circle</v-icon
+                >
+                <v-icon v-else size="30" color="#FFFFFF"
+                  >mdi-checkbox-blank-circle</v-icon
+                >
+              </v-list-item-action>
+              <div class="tasklistTaskNames restructuredSubTaskName">
+                <div class="body-2">
+                  <span class="restructuredMainTaskCode">MRI - #1</span>
+                  {{ childTask.taskName }}
+                </div>
               </div>
-            </div>
-            <div class="restStatusChip" :class="statusCheck(task)">{{childTask.issueType}}</div>
-            <v-list-item-content class="updatedDate">
-              <v-list-item-title :class="dueDateCheck(task)">
-                {{
-                getProjectDates(childTask.taskDueDateAt)
-                }}
-              </v-list-item-title>
-            </v-list-item-content>
-            <v-list-item-avatar>
-              <v-img
-                v-if="childTask.taskAssigneeProfileImage != null"
-                :src="childTask.taskAssigneeProfileImage"
-              ></v-img>
-              <v-img
-                v-else
-                src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png"
-              ></v-img>
-            </v-list-item-avatar>
-          </v-list-item>
+              <div class="restStatusChip" :class="statusCheck(task)">
+                {{ childTask.issueType }}
+              </div>
+              <v-list-item-content class="updatedDate">
+                <v-list-item-title :class="dueDateCheck(task)">
+                  {{ getProjectDates(childTask.taskDueDateAt) }}
+                </v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-avatar>
+                <v-img
+                  v-if="childTask.taskAssigneeProfileImage != null"
+                  :src="childTask.taskAssigneeProfileImage"
+                ></v-img>
+                <v-img
+                  v-else
+                  src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png"
+                ></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+          </div>
         </div>
 
         <!-- -------------- end sub task design -------------- -->
@@ -241,8 +261,8 @@
 </template>
 
 <script>
-import TaskSideBar from "~/components/tasks/taskSideBar";
-import { mapState } from "vuex";
+import TaskSideBar from '~/components/tasks/taskSideBar';
+import { mapState } from 'vuex';
 export default {
   // props: ['projectId', 'projectUsers', 'people'],
   data() {
@@ -254,76 +274,76 @@ export default {
       widgets: false,
       states: [],
       items: [
-        { id: "all", name: "All" },
-        { id: "pending", name: "Pending" },
-        { id: "implementing", name: "Implementing" },
-        { id: "qa", name: "QA" },
-        { id: "readyToDeploy", name: "Ready to deploy" },
-        { id: "reOpened", name: "Reopened" },
-        { id: "deployed", name: "Deployed" },
-        { id: "closed", name: "Closed" }
+        { id: 'all', name: 'All' },
+        { id: 'pending', name: 'Pending' },
+        { id: 'implementing', name: 'Implementing' },
+        { id: 'qa', name: 'QA' },
+        { id: 'readyToDeploy', name: 'Ready to deploy' },
+        { id: 'reOpened', name: 'Reopened' },
+        { id: 'deployed', name: 'Deployed' },
+        { id: 'closed', name: 'Closed' },
       ],
       filterOptions: [
-        { id: "none", name: "None" },
-        { id: "assignee", name: "Assignee" },
-        { id: "type", name: "Type" },
-        { id: "dateRange", name: "Date Range" }
+        { id: 'none', name: 'None' },
+        { id: 'assignee', name: 'Assignee' },
+        { id: 'type', name: 'Type' },
+        { id: 'dateRange', name: 'Date Range' },
       ],
-      projects: ["pr1"],
+      projects: ['pr1'],
       drawer: null,
       task: {},
       subTasks: [],
       taskFiles: [],
       assignee: {},
       userId: this.$store.state.user.userId,
-      taskSelect: "all",
-      taskFilter: "none",
-      componentClose: null
+      taskSelect: 'all',
+      taskFilter: 'none',
+      componentClose: null,
     };
   },
   components: {
-    "task-side-bar": TaskSideBar
+    'task-side-bar': TaskSideBar,
   },
   methods: {
     clearFilter() {
-      console.log("selected===========> " + this.taskSelect);
+      console.log('selected===========> ' + this.taskSelect);
       this.taskSelect == null;
     },
     querySelections(v) {
-      console.log("people list", this.people);
+      console.log('people list', this.people);
       this.states = [];
       let projectSearchList = this.people;
       for (let index = 0; index < projectSearchList.length; ++index) {
         let user = projectSearchList[index];
         this.states.push({
-          name: user.assigneeFirstName + " " + user.assigneeLastName,
+          name: user.assigneeFirstName + ' ' + user.assigneeLastName,
           id: user,
-          img: user.assigneeProfileImage
+          img: user.assigneeProfileImage,
         });
       }
-      console.log("nameList", this.states);
+      console.log('nameList', this.states);
       this.loading = true;
     },
     listenToChange() {
-      console.log("listened to changes ------->");
-      this.$store.dispatch("task/fetchTasksAllTasks", this.projectId);
-      this.$store.dispatch("task/fetchTasksMyTasks", this.projectId);
-      this.$store.dispatch("task/fetchProjectTaskCompletion", this.projectId);
+      console.log('listened to changes ------->');
+      this.$store.dispatch('task/fetchTasksAllTasks', this.projectId);
+      this.$store.dispatch('task/fetchTasksMyTasks', this.projectId);
+      this.$store.dispatch('task/fetchProjectTaskCompletion', this.projectId);
     },
     shrinkSideBar() {
       this.drawer = false;
     },
     taskFilterHandler() {
-      console.log("-----------> changed" + this.taskSelect);
+      console.log('-----------> changed' + this.taskSelect);
     },
     async selectTask(task) {
       this.task = task.parentTask;
-      this.componentClose = "";
-      console.log("selectedTask", task);
+      this.componentClose = '';
+      console.log('selectedTask', task);
       this.$axios
         .get(`/users/${this.task.taskAssignee}`)
-        .then(async response => {
-          console.log("fetched task -->", response.data.data);
+        .then(async (response) => {
+          console.log('fetched task -->', response.data.data);
           this.assignee = response.data.data;
           //if task fetch is successful,
           let subTaskResponse;
@@ -332,11 +352,11 @@ export default {
               `/projects/${this.projectId}/tasks/${task.taskId}/subtask?userId=${this.userId}`,
               {
                 headers: {
-                  type: "project"
-                }
+                  type: 'project',
+                },
               }
             );
-            console.log("subtasks--->", subTaskResponse.data);
+            console.log('subtasks--->', subTaskResponse.data);
             this.subTasks = subTaskResponse.data;
             //get files related to task
             let taskFilesResponse;
@@ -346,84 +366,84 @@ export default {
                 {
                   headers: {
                     user: this.userId,
-                    type: "project"
-                  }
+                    type: 'project',
+                  },
                 }
               );
-              console.log("files--->", taskFilesResponse.data);
+              console.log('files--->', taskFilesResponse.data);
               this.taskFiles = taskFilesResponse.data;
             } catch (error) {
-              console.log("Error fetching data", error);
+              console.log('Error fetching data', error);
             }
           } catch (error) {
-            console.log("Error fetching data", error);
+            console.log('Error fetching data', error);
           }
         })
-        .catch(e => {
-          console.log("error", e);
+        .catch((e) => {
+          console.log('error', e);
         });
     },
     statusCheck(task) {
-      if (task.taskStatus === "closed") {
-        return "closedStatus";
+      if (task.taskStatus === 'closed') {
+        return 'closedStatus';
       } else {
-        return "otherStatus";
+        return 'otherStatus';
       }
     },
     dueDateCheck(task) {
-      console.log("check due date color", task);
-      if (task.taskStatus === "closed") {
-        return "workLoadTaskDone";
+      console.log('check due date color', task);
+      if (task.taskStatus === 'closed') {
+        return 'workLoadTaskDone';
       } else if (task.taskDueDateAt == null) {
-        return "workLoadTaskDefault";
+        return 'workLoadTaskDefault';
       } else {
         const dueDate = new Date(task.taskDueDateAt);
         const dueToUtc = new Date(
-          dueDate.toLocaleString("en-US", { timeZone: "UTC" })
+          dueDate.toLocaleString('en-US', { timeZone: 'UTC' })
         );
         const dueToUtcDate = new Date(dueToUtc);
         const now = new Date();
-        console.log("now", now.getTime(), "DueTime", dueToUtcDate.getTime());
+        console.log('now', now.getTime(), 'DueTime', dueToUtcDate.getTime());
         if (now.getTime() > dueToUtcDate.getTime()) {
-          console.log("overdue");
-          return "workLoadTaskOverDue";
+          console.log('overdue');
+          return 'workLoadTaskOverDue';
         } else {
-          return "workLoadTaskHealthy";
+          return 'workLoadTaskHealthy';
         }
       }
     },
     getProjectDates(date) {
       const dueDate = new Date(date);
       const dueToUtc = new Date(
-        dueDate.toLocaleString("en-US", { timeZone: "UTC" })
+        dueDate.toLocaleString('en-US', { timeZone: 'UTC' })
       );
       const dueToUtcDate = new Date(dueToUtc);
       const now = new Date();
-      console.log("Today", now.getDate(), "DueDate", dueToUtcDate.getDate());
+      console.log('Today', now.getDate(), 'DueDate', dueToUtcDate.getDate());
 
-      if (date === null || date === "1970-01-01T05:30:00.000+0000") {
-        return "Add Due Date";
+      if (date === null || date === '1970-01-01T05:30:00.000+0000') {
+        return 'Add Due Date';
       } else if (now.getDate() === dueToUtcDate.getDate()) {
-        return "Today";
+        return 'Today';
       } else if (now.getDate() - 1 === dueToUtcDate.getDate()) {
-        return "Yesterday";
+        return 'Yesterday';
       } else if (now.getDate() + 1 === dueToUtcDate.getDate()) {
-        return "Tomorrow";
+        return 'Tomorrow';
       } else {
-        let stringDate = date + "";
+        let stringDate = date + '';
         stringDate = stringDate.toString();
         stringDate = stringDate.slice(0, 10);
         return stringDate;
       }
-    }
+    },
   },
   computed: {
     ...mapState({
-      people: state => state.task.userCompletionTasks,
-      projectAllTasks: state => state.task.allTasks,
-      projectId: state => state.project.project.projectId
-    })
-  }
+      people: (state) => state.task.userCompletionTasks,
+      projectAllTasks: (state) => state.task.allTasks,
+      projectId: (state) => state.project.project.projectId,
+    }),
+  },
 };
 </script>
 
