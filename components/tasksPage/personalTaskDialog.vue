6ebd7af3c02d123de0fc2fa3754b492e850fc332
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-05-14 19:13:27 +0530

    updated

--- components/tasksPage/personalTaskDialog.vue
+++ components/tasksPage/personalTaskDialog.vue
@@ -489,12 +489,6 @@ export default {
 
       if (date === null || date === "1970-01-01T05:30:00.000+0000") {
         return "Add Task Date";
-      } else if (now.getDate() === dueToUtcDate.getDate()) {
-        return "Today";
-      } else if (now.getDate() - 1 === dueToUtcDate.getDate()) {
-        return "Yesterday";
-      } else if (now.getDate() + 1 === dueToUtcDate.getDate()) {
-        return "Tomorrow";
       } else {
         let stringDate = date + "";
         stringDate = stringDate.toString();
@@ -809,6 +803,12 @@ export default {
 
       if (date === null || date === "1970-01-01T05:30:00.000+0000") {
         return "Add Due Date";
+      } else if (now.getDate() === dueToUtcDate.getDate()) {
+        return "Today";
+      } else if (now.getDate() - 1 === dueToUtcDate.getDate()) {
+        return "Yesterday";
+      } else if (now.getDate() + 1 === dueToUtcDate.getDate()) {
+        return "Tomorrow";
       } else {
         let stringDate = date + "";
         stringDate = stringDate.toString();
