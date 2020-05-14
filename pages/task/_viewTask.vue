8c5354e2c8ffeb181c439b61229cc6c21d27db1f
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-05-14 21:12:40 +0530

    sprint updated

--- pages/task/_viewTask.vue
+++ pages/task/_viewTask.vue
@@ -455,6 +455,21 @@
                         <v-row class="mb-12" no-gutters>
                           <v-col sm="12" md="12">
                             <v-select
+                              v-if=" selectedTask.isParent == true"
+                              dense
+                              v-model="selectedSprint"
+                              :items="getAllSprints()"
+                              background-color="#EDF0F5"
+                              item-text="name"
+                              item-value="id"
+                              label="Board"
+                              outlined
+                              class="createFormElements"
+                              @change="changeTaskSprint"
+                            ></v-select>
+                            <v-select
+                              v-else
+                              disabled
                               dense
                               v-model="selectedSprint"
                               :items="getAllSprints()"
