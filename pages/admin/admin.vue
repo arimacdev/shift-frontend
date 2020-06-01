8e4f9e18144bfc1f698381eca6929fdd17f8cd33
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-06-01 08:50:25 +0530

    admin page added

--- pages/admin/admin.vue
+++ pages/admin/admin.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="top-nav">
     <navigation-drawer :user="user" />
+
+    <v-toolbar app color dark fixed :clipped-left="clipped" class="toolBarFilter tool-bar">
+      <div class="title-div">
+        <div class="name-div">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold">Admin</v-list-item-title>
+            </v-list-item-content>
+
+            <v-divider class="mx-4" inset vertical></v-divider>
+          </v-list-item>
+        </div>
+      </div>
+    </v-toolbar>
+
+    <div class="body-div">
+      <div class="workloadTypeSection">
+        <v-tabs background-color="#0b0b53" dark>
+          <v-tab v-on:click="component='my-workload' ">Users</v-tab>
+          <v-tab v-on:click="component='org-workload'">Organization</v-tab>
+        </v-tabs>
+      </div>
+    </div>
   </div>
 </template>
 <script>
