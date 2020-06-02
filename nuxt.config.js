90e575611878df4b357a665316b2383e3a2b288e
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-06-02 10:28:15 +0530

    nuxt updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 import colors from 'vuetify/es5/util/colors';
+
 export default {
   mode: 'universal',
   /*
@@ -42,17 +43,6 @@ export default {
    ** Nuxt.js modules
    */
   modules: ['@nuxtjs/axios', '@nuxtjs/auth'],
-  env: {
-    SLACK_AUTHORIZATION_ENDPOINT:
-      process.env.SLACK_AUTHORIZATION_ENDPOINT ||
-      'https://slack.com/oauth/v2/authorize',
-    SLACK_TOKEN_ENDPOINT:
-      process.env.SLACK_TOKEN_ENDPOINT || 'SLACK_TOKEN_ENDPOINT',
-    SLACK_CLIENT_ID:
-      process.env.SLACK_CLIENT_ID || '345426929140.1020110511447',
-    SLACK_CLIENT_SECRET:
-      process.env.SLACK_CLIENT_SECRET || 'fd851b7af77e525c1700879de9b328ab',
-  },
   /*
    ** Base URL
    ** http://pmtool.devops.arimac.xyz/api/pm-service
@@ -61,28 +51,31 @@ export default {
    ** 138bbb3d-02ed-4d72-9a03-7e8cdfe89eff
    */
   axios: {
-    baseURL: 'http://localhost:8080/api/pm-service',
+    baseURL: `${process.env.BASE_URL}/api/pm-service`,
   },
   /*
    ** vuetify module configuration
    ** https://github.com/nuxt-community/vuetify-module
    */
+
+  env: {
+    ORGANIZATION_URL:
+      process.env.ORGANIZATION_URL || 'https://project.arimaclanka.com',
+  },
+
   auth: {
     strategies: {
       local: false,
       keycloak: {
         _scheme: 'oauth2',
-        authorization_endpoint:
-          'https://pmtool.devops.arimac.xyz/auth/realms/pm-tool/protocol/openid-connect/auth',
-        userinfo_endpoint:
-          'https://pmtool.devops.arimac.xyz/auth/realms/pm-tool/protocol/openid-connect/userinfo',
-        access_token_endpoint:
-          'https://pmtool.devops.arimac.xyz/auth/realms/pm-tool/protocol/openid-connect/token',
+        authorization_endpoint: `${process.env.BASE_URL}/auth/realms/pm-tool/protocol/openid-connect/auth`,
+        userinfo_endpoint: `${process.env.BASE_URL}/auth/realms/pm-tool/protocol/openid-connect/userinfo`,
+        access_token_endpoint: `${process.env.BASE_URL}/auth/realms/pm-tool/protocol/openid-connect/token`,
         scope: ['openid', 'roles', 'profile'],
         grant_type: 'authorization_code',
         response_type: 'code',
         token_type: 'Bearer',
-        client_id: 'pmtool-frontend',
+        client_id: `${process.env.KEYCLOAK_CLIENT_ID}`,
         token_key: 'access_token',
       },
     },
@@ -93,14 +86,17 @@ export default {
     },
     localStorage: false,
   },
+
   router: {
     middleware: ['auth', 'token'],
   },
+
   constants: {
-    hostUrl: 'https://pmtool.devops.arimac.xyz',
-    appUrl: 'https://pmtool.devops.arimac.xyz',
-    realm: 'pm-tool',
+    hostUrl: `${process.env.BASE_URL}`,
+    appUrl: `${process.env.BASE_URL}`,
+    realm: `${process.env.KEYCLOAK_REALM}`,
   },
+
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
