18f12283aef796cd5bdcd5afd04fefdba385514e
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-06-11 15:28:10 +0530

    skill matrix updated

---
+++ components/admin/skills.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="tabContent">
+      <div class="users-list">
+        <v-row>
+          <v-col>
+            <button class="addUserSubmitButtonEdit userAddBottom">
+              <v-list-item dark>
+                <v-list-item-icon>
+                  <v-icon size="20" color>mdi-thumb-up-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content class="userAddButtonText">
+                  <v-list-item-title class>Add new skill category</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </button>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="userAddBottom">
+            <search-bar :users="users" @searchSelected="viewSearchComponent" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <div class="listView overflow-y-auto">add skills here</div>
+          </v-col>
+        </v-row>
+      </div>
+      <div>
+        <keep-alive>
+          <skills-content />
+        </keep-alive>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import usersSearchBar from "~/components/tools/usersSearchBar";
+
+import skillsContent from "~/components/admin/skillsContent";
+import { mapState } from "vuex";
+export default {
+  components: {
+    "search-bar": usersSearchBar,
+    "skills-content": skillsContent
+  }
+};
+</script>
---
+++ components/admin/skillsContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="skillsContentWrapper">
+    <v-row>
+      <v-col>Skill Category Name</v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <span class="display-1 font-weight-bold">Front End</span>
+        &nbsp;
+        <v-divider vertical></v-divider>
+        <span style="margin-left: 30px; margin-top: -20px">Color</span>
+
+        <v-btn style="margin-left: 30px; margin-top: -20px" color="error">Delete Skill Category</v-btn>
+      </v-col>
+    </v-row>
+
+    <v-divider style="margin-top: 20px"></v-divider>
+  </div>
+</template>
