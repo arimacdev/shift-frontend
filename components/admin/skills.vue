77e7a53d3f9de20533fb72ee05cbb0068d95f224
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-06-11 17:33:17 +0530

    color picker added

--- components/admin/skills.vue
+++ components/admin/skills.vue
@@ -4,7 +4,11 @@
       <div class="users-list">
         <v-row>
           <v-col>
-            <v-btn height="50px" class="addUserSubmitButtonEdit userAddBottom">
+            <v-btn
+              @click="skillDialog = true"
+              height="50px"
+              class="addUserSubmitButtonEdit userAddBottom"
+            >
               <v-list-item dark>
                 <v-list-item-icon>
                   <v-icon size="20" color>mdi-thumb-up-outline</v-icon>
@@ -41,6 +45,38 @@
         </keep-alive>
       </div>
     </div>
+
+    <!-- ------- add skill cat dialog ------ -->
+
+    <v-dialog v-model="skillDialog" max-width="450">
+      <v-card style=" padding-bottom: 25px">
+        <v-card-title style="text-align: center">
+          <v-spacer></v-spacer>Add Skill category
+          <v-spacer></v-spacer>
+        </v-card-title>
+
+        <v-card-text>
+          <v-text-field
+            v-model="categoryName"
+            outlined
+            background-color="#EDF0F5"
+            label="Skill category name"
+          ></v-text-field>
+          <span style="text-align: left !important">Pick a color</span>
+          {{this.colorPicker}}
+          <v-color-picker width="100%" v-model="colorPicker" hide-canvas class="ma-2" show-swatches></v-color-picker>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn width="100px" color="#FF6161" dark @click="skillDialog = false">Cancel</v-btn>
+
+          <v-btn width="100px" color="#2EC973" dark @click="skillDialog = false">Ok</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -53,6 +89,21 @@ export default {
   components: {
     "search-bar": usersSearchBar,
     "skills-content": skillsContent
+  },
+  data() {
+    return {
+      skillDialog: false,
+      colorPicker: "",
+      categoryName: "",
+      swatches: [
+        ["#FF0000", "#AA0000", "#550000"],
+        ["#FFFF00", "#AAAA00", "#555500"],
+        ["#00FF00", "#00AA00", "#005500"],
+        ["#00FFFF", "#00AAAA", "#005555"],
+        ["#0099FFFF", "#FF008CFF", "#00F7FFFF"],
+        ["#0000FF", "#0000AA", "#000055"]
+      ]
+    };
   }
 };
 </script>
