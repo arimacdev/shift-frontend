c913237a9ab04f78b955b934bd70a36c8ba578f2
Naveen <naveenperera777@gmail.com>
2020-06-15 17:37:16 +0530

    OneSignal Setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 import colors from 'vuetify/es5/util/colors';
-
 export default {
   mode: 'universal',
   /*
@@ -8,6 +7,7 @@ export default {
   head: {
     titleTemplate: '%s ',
     title: 'SHIFT - Arimac Digital',
+    __dangerouslyDisableSanitizers: ['script'],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -17,7 +17,21 @@ export default {
         content: process.env.npm_package_description || '',
       },
     ],
+    // allowLocalhostAsSecureOrigin: true,
+    
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    // script: [{src: '/onesignal.js'}]
+    script:[{src: 'https://cdn.onesignal.com/sdks/OneSignalSDK.js', async:""}, {innerHTML: `window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+      OneSignal.init({
+        appId: "caab97bf-359f-4c09-91ad-4a6752ce1667",
+        notifyButton: {
+          enable: false,
+        },
+        persistNotification: false 
+      })
+      OneSignal.showNativePrompt();
+    });`, type: 'text/javascript',  charset: 'utf-8'}]
   },
   /*
    ** Customize the progress-bar color
@@ -33,7 +47,7 @@ export default {
   plugins: [
     '~/plugins/vuelidate.js',
     '~/plugins/datepicker.js',
-    '~/plugins/vuedatetime.js',
+    '~/plugins/vuedatetime.js'
   ],
   /*
    ** Nuxt.js dev-modules
@@ -43,6 +57,19 @@ export default {
    ** Nuxt.js modules
    */
   modules: ['@nuxtjs/axios', '@nuxtjs/auth'],
+  env: {
+    SLACK_AUTHORIZATION_ENDPOINT:
+      process.env.SLACK_AUTHORIZATION_ENDPOINT ||
+      'https://slack.com/oauth/v2/authorize',
+    SLACK_TOKEN_ENDPOINT:
+      process.env.SLACK_TOKEN_ENDPOINT || 'SLACK_TOKEN_ENDPOINT',
+    SLACK_CLIENT_ID:
+      process.env.SLACK_CLIENT_ID || '345426929140.1020110511447',
+    SLACK_CLIENT_SECRET:
+      process.env.SLACK_CLIENT_SECRET || 'fd851b7af77e525c1700879de9b328ab',
+    ORGANIZATION_URL:
+      process.env.ORGANIZATION_URL || 'https://project.arimaclanka.com',
+  },
   /*
    ** Base URL
    ** http://pmtool.devops.arimac.xyz/api/pm-service
@@ -51,28 +78,35 @@ export default {
    ** 138bbb3d-02ed-4d72-9a03-7e8cdfe89eff
    */
   axios: {
-    baseURL: `${process.env.BASE_URL}/api/pm-service`,
-  },
-  env: {
-    SYSTEM_URL: process.env.BASE_URL || 'https://project.arimaclanka.com',
+    baseURL: 'http://localhost:8080/api/pm-service',
   },
   /*
    ** vuetify module configuration
    ** https://github.com/nuxt-community/vuetify-module
    */
+  env: {
+    SYSTEM_URL: process.env.BASE_URL || 'http://localhost:3000',
+
+    LOGOUT_URL:
+      process.env.LOGOUT_URL ||
+      'https://project.arimaclanka.com/auth/realms/pm-tool/protocol/openid-connect/logout',
+  },
   auth: {
     strategies: {
       local: false,
       keycloak: {
         _scheme: 'oauth2',
-        authorization_endpoint: `${process.env.BASE_URL}/auth/realms/pm-tool/protocol/openid-connect/auth`,
-        userinfo_endpoint: `${process.env.BASE_URL}/auth/realms/pm-tool/protocol/openid-connect/userinfo`,
-        access_token_endpoint: `${process.env.BASE_URL}/auth/realms/pm-tool/protocol/openid-connect/token`,
+        authorization_endpoint:
+          'https://pmtool.devops.arimac.xyz/auth/realms/pm-tool/protocol/openid-connect/auth',
+        userinfo_endpoint:
+          'https://pmtool.devops.arimac.xyz/auth/realms/pm-tool/protocol/openid-connect/userinfo',
+        access_token_endpoint:
+          'https://pmtool.devops.arimac.xyz/auth/realms/pm-tool/protocol/openid-connect/token',
         scope: ['openid', 'roles', 'profile'],
         grant_type: 'authorization_code',
         response_type: 'code',
         token_type: 'Bearer',
-        client_id: `${process.env.KEYCLOAK_CLIENT_ID}`,
+        client_id: 'pmtool-frontend',
         token_key: 'access_token',
       },
     },
@@ -83,17 +117,14 @@ export default {
     },
     localStorage: false,
   },
-
   router: {
     middleware: ['auth', 'token'],
   },
-
   constants: {
-    hostUrl: `${process.env.BASE_URL}`,
-    appUrl: `${process.env.BASE_URL}`,
-    realm: `${process.env.KEYCLOAK_REALM}`,
+    hostUrl: 'https://pmtool.devops.arimac.xyz',
+    appUrl: 'https://pmtool.devops.arimac.xyz',
+    realm: 'pm-tool',
   },
-
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
