8c10a0bc4bfd829b2e05a293c8ce4c02c7b4bf1b
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-06-22 13:53:27 +0530

    scroll bar added

--- components/profile/profileContent.vue
+++ components/profile/profileContent.vue
@@ -3,7 +3,7 @@
     <div class="blackBar profileBlackBar"></div>
     <div class="userImage profileUserImage">
       <v-img
-        v-if="user.profileImage != null && user.profileImage != ''  "
+        v-if="user.profileImage != null && user.profileImage != ''"
         class="profileImage"
         :src="user.profileImage"
       ></v-img>
@@ -41,13 +41,22 @@
               <v-icon right dark>mdi-upload</v-icon>
             </v-btn>
 
-            <v-progress-circular v-if="uploadLoading == true" indeterminate color="primary"></v-progress-circular>
+            <v-progress-circular
+              v-if="uploadLoading == true"
+              indeterminate
+              color="primary"
+            ></v-progress-circular>
           </div>
         </form>
 
         <!-- ----------------------- slack --------------------- -->
 
-        <v-card class="mx-auto slackCard" max-width="344" height="220px" outlined>
+        <v-card
+          class="mx-auto slackCard"
+          max-width="344"
+          height="220px"
+          outlined
+        >
           <v-img
             class="white--text align-end slackImage"
             width="100px"
@@ -64,7 +73,10 @@
                 height
                 width="120"
                 src="https://arimac-pmtool.s3-ap-southeast-1.amazonaws.com/projectFile_1592584747849_add_to_slack.png"
-                srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+                srcset="
+                  https://platform.slack-edge.com/img/add_to_slack.png    1x,
+                  https://platform.slack-edge.com/img/add_to_slack@2x.png 2x
+                "
               />
             </a>
           </div>
@@ -75,7 +87,9 @@
             <!-- <v-btn x-small depressed color="primary" v-if="user.userSlackId != null && user.notification == false" v-show="enableNotification"  @click='changeNotificationStatus(user.notification)' >Enable Notifications</v-btn>  
             <v-btn x-small depressed   v-if="user.userSlackId != null && user.notification == true" v-show="disableNotification"  @click='changeNotificationStatus(user.notification)'>Disable Notifications</v-btn>-->
 
-            <div class="notiTitle" v-if="user.userSlackId != null">Enable Notifications</div>
+            <div class="notiTitle" v-if="user.userSlackId != null">
+              Enable Notifications
+            </div>
             <div class="notiButton">
               <v-switch
                 inset
@@ -86,7 +100,8 @@
                 v-if="user.userSlackId != null && user.notification == false"
                 v-show="enableNotification"
                 @click="changeNotificationStatus(user.notification)"
-              >Enable Notifications</v-switch>
+                >Enable Notifications</v-switch
+              >
               <v-switch
                 inset
                 v-model="switch1"
@@ -96,7 +111,8 @@
                 v-if="user.userSlackId != null && user.notification == true"
                 v-show="disableNotification"
                 @click="changeNotificationStatus(user.notification)"
-              >Disable Notifications</v-switch>
+                >Disable Notifications</v-switch
+              >
             </div>
           </div>
         </v-card>
@@ -143,7 +159,9 @@
       </div>
     </div>
     <v-form @submit.prevent="handleSubmit" v-model="isValid" ref="form">
-      <div class="profileUserName">{{ user.firstName }} {{ user.lastName }}</div>
+      <div class="profileUserName">
+        {{ user.firstName }} {{ user.lastName }}
+      </div>
 
       <div class="userDetails">
         <p class="userName"></p>
@@ -213,7 +231,9 @@
             <div
               v-if="$v.password.$error && !$v.password.minLength"
               class="errorText"
-            >Password must be at least 6 characters</div>
+            >
+              Password must be at least 6 characters
+            </div>
           </v-col>
           <v-col sm="6" md="6">
             <v-text-field
@@ -227,13 +247,19 @@
             <div
               v-if="$v.confirmPassword.$error && !$v.confirmPassword.sameAs"
               class="errorText"
-            >Passwords must be identical</div>
+            >
+              Passwords must be identical
+            </div>
           </v-col>
         </v-row>
         <v-row>
           <v-col sm="12" md="12">
             <v-btn
-              v-if="this.password != '' && !this.$v.$invalid == true && isValid == true"
+              v-if="
+                this.password != '' &&
+                  !this.$v.$invalid == true &&
+                  isValid == true
+              "
               height="50px"
               width="300px"
               class="submitButtonEdit"
@@ -243,7 +269,9 @@
                   <v-icon size="20" color>icon-user</v-icon>
                 </v-list-item-action>
                 <v-list-item-content class="buttonText">
-                  <v-list-item-title class="bodyWiew">Edit profile details</v-list-item-title>
+                  <v-list-item-title class="bodyWiew"
+                    >Edit profile details</v-list-item-title
+                  >
                 </v-list-item-content>
                 <!-- <div class="iconBackCircle">
                   <v-icon size="17" color="#0BAFFF">mdi-pencil-outline</v-icon>
@@ -252,7 +280,12 @@
             </v-btn>
 
             <v-btn
-              v-else-if="this.confirmPassword == '' && this.password == '' && !this.$v.$invalid == false && isValid == true"
+              v-else-if="
+                this.confirmPassword == '' &&
+                  this.password == '' &&
+                  !this.$v.$invalid == false &&
+                  isValid == true
+              "
               height="50px"
               width="300px"
               class="submitButtonEdit"
@@ -262,17 +295,27 @@
                   <v-icon size="20" color>icon-user</v-icon>
                 </v-list-item-action>
                 <v-list-item-content class="buttonText">
-                  <v-list-item-title class="bodyWiew">Edit profile details</v-list-item-title>
+                  <v-list-item-title class="bodyWiew"
+                    >Edit profile details</v-list-item-title
+                  >
                 </v-list-item-content>
               </v-list-item>
             </v-btn>
-            <v-btn v-else disabled height="50px" width="300px" class="submitButtonEdit">
+            <v-btn
+              v-else
+              disabled
+              height="50px"
+              width="300px"
+              class="submitButtonEdit"
+            >
               <v-list-item @click="postData()" dark>
                 <v-list-item-action>
                   <v-icon size="20" color>icon-user</v-icon>
                 </v-list-item-action>
                 <v-list-item-content class="buttonText">
-                  <v-list-item-title class="bodyWiew">Edit profile details</v-list-item-title>
+                  <v-list-item-title class="bodyWiew"
+                    >Edit profile details</v-list-item-title
+                  >
                 </v-list-item-content>
               </v-list-item>
             </v-btn>
@@ -287,7 +330,7 @@
         <v-row class="skillsSection">
           <v-col>
             <div class="skillDisplayDiv">
-              <div class="skillScrollingWrapper">
+              <div class="skillProfileScrollingWrapper">
                 <div
                   class="skillCard text-center"
                   v-for="(category, index) in userSkillMap"
@@ -296,14 +339,18 @@
                   <div
                     class="skillHeader"
                     :style="'background-color:' + category.categoryColorCode"
-                  >{{category.categoryName}}</div>
+                  >
+                    {{ category.categoryName }}
+                  </div>
 
                   <div class="skillBody">
                     <div
                       style="padding-bottom: 10px"
                       v-for="(skill, index) in category.skillSet"
                       :key="index"
-                    >{{skill.skillName}}</div>
+                    >
+                      {{ skill.skillName }}
+                    </div>
                   </div>
                 </div>
               </div>
@@ -324,22 +371,21 @@
   </div>
 </template>
 
-
 <script>
-import EditProfile from "~/components/profile/editProfile";
-import axios from "axios";
-import qs from "qs";
-import { mapState } from "vuex";
-import { required, minLength, sameAs } from "vuelidate/lib/validators";
-import SuccessPopup from "~/components/popups/successPopup";
-import ErrorPopup from "~/components/popups/errorPopup";
+import EditProfile from '~/components/profile/editProfile';
+import axios from 'axios';
+import qs from 'qs';
+import { mapState } from 'vuex';
+import { required, minLength, sameAs } from 'vuelidate/lib/validators';
+import SuccessPopup from '~/components/popups/successPopup';
+import ErrorPopup from '~/components/popups/errorPopup';
 
 export default {
-  props: ["user"],
+  props: ['user'],
   components: {
-    "edit-profile": EditProfile,
-    "success-popup": SuccessPopup,
-    "error-popup": ErrorPopup
+    'edit-profile': EditProfile,
+    'success-popup': SuccessPopup,
+    'error-popup': ErrorPopup,
   },
   // data: function(){
   // return{
@@ -349,11 +395,11 @@ export default {
   data() {
     return {
       isValid: true,
-      firstNameRules: [value => !!value || "First name is required!"],
-      lastNameRules: [value => !!value || "Last name is required!"],
+      firstNameRules: [(value) => !!value || 'First name is required!'],
+      lastNameRules: [(value) => !!value || 'Last name is required!'],
       emailRules: [
-        value => !!value || "E-mail is required",
-        value => /.+@.+\..+/.test(value) || "E-mail must be valid"
+        (value) => !!value || 'E-mail is required',
+        (value) => /.+@.+\..+/.test(value) || 'E-mail must be valid',
       ],
 
       disableButton: true,
@@ -361,8 +407,8 @@ export default {
       switch2: false,
       uploadLoading: false,
       files: [],
-      errorMessage: "",
-      successMessage: "",
+      errorMessage: '',
+      successMessage: '',
       userName: this.user.userName,
       firstName: this.user.firstName,
       lastName: this.user.lastName,
@@ -371,13 +417,13 @@ export default {
       disableNotification: !this.user.notificationStatus,
       loader: null,
       loading: false,
-      password: "",
-      confirmPassword: "",
-      file: "",
+      password: '',
+      confirmPassword: '',
+      file: '',
       userId: this.$store.state.user.userId,
       dismissSecs: 5,
       dismissCountDown: 0,
-      component: ""
+      component: '',
     };
   },
   watch: {
@@ -388,13 +434,13 @@ export default {
       setTimeout(() => (this[l] = false), 3000);
 
       this.loader = null;
-    }
+    },
   },
 
   computed: {
     ...mapState({
-      userSkillMap: state => state.skillMap.userSkillMap
-    })
+      userSkillMap: (state) => state.skillMap.userSkillMap,
+    }),
   },
 
   created: function() {
@@ -403,18 +449,18 @@ export default {
     if (authCode !== undefined) {
       // console.log("Slack Auth code present");
       axios({
-        method: "post",
-        url: "https://slack.com/api/oauth.v2.access",
+        method: 'post',
+        url: 'https://slack.com/api/oauth.v2.access',
         data: qs.stringify({
-          client_id: "345426929140.1020110511447",
-          client_secret: "fd851b7af77e525c1700879de9b328ab",
-          code: authCode
+          client_id: '345426929140.1020110511447',
+          client_secret: 'fd851b7af77e525c1700879de9b328ab',
+          code: authCode,
         }),
         headers: {
-          "content-type": "application/x-www-form-urlencoded"
-        }
+          'content-type': 'application/x-www-form-urlencoded',
+        },
       })
-        .then(async resp => {
+        .then(async (resp) => {
           // console.log("Slack Token Response", resp.data);
           const slackId = resp.data.authed_user.id;
           // console.log("User Slack Id", slackId);
@@ -425,24 +471,24 @@ export default {
               {
                 slackAssignerId: this.userId,
                 slackAssigneeId: this.user.userId,
-                assigneeSlackId: slackId
+                assigneeSlackId: slackId,
               },
               {
                 headers: {
-                  userId: this.userId
-                }
+                  userId: this.userId,
+                },
               }
             );
-            console.log("Slack Id saved successfuly", response);
+            console.log('Slack Id saved successfuly', response);
           } catch (e) {
-            console.log("Error saving slack Id in database", e);
+            console.log('Error saving slack Id in database', e);
           }
         })
-        .catch(e => {
-          console.log("error from slack", e);
+        .catch((e) => {
+          console.log('error from slack', e);
         });
     } else {
-      console.log("No Slack Auth Code Present");
+      console.log('No Slack Auth Code Present');
     }
   },
 
@@ -487,7 +533,7 @@ export default {
       return orderedSkillMap;
     },
     setVisible() {
-      console.log("DISABLED!");
+      console.log('DISABLED!');
 
       this.disableButton = false;
     },
@@ -499,12 +545,12 @@ export default {
           {
             slackAssignerId: this.userId,
             slackAssigneeId: this.user.userId,
-            notificationStatus: !status
+            notificationStatus: !status,
           },
           {
             headers: {
-              userId: this.userId
-            }
+              userId: this.userId,
+            },
           }
         );
         location.reload();
@@ -519,39 +565,39 @@ export default {
         }
         // this.enableNotification = ;
         // this.disableNotification = ;
-        console.log("Notification Status updated successfuly", response);
+        console.log('Notification Status updated successfuly', response);
       } catch (e) {
-        console.log("Error Updating Notification Status", e);
+        console.log('Error Updating Notification Status', e);
       }
     },
 
     async postData() {
       let response;
       try {
-        if (this.password == "") {
+        if (this.password == '') {
           response = await this.$axios.$put(`/users/${this.userId}`, {
             firstName: this.user.firstName,
             lastName: this.user.lastName,
-            email: this.user.email
+            email: this.user.email,
           });
         } else {
           response = await this.$axios.$put(`/users/${this.userId}`, {
             firstName: this.user.firstName,
             lastName: this.user.lastName,
             email: this.user.email,
-            password: this.password
+            password: this.password,
           });
         }
 
-        this.component = "success-popup";
-        this.successMessage = "Profile successfully updated";
+        this.component = 'success-popup';
+        this.successMessage = 'Profile successfully updated';
         setTimeout(() => {
           this.close();
         }, 3000);
         // console.log(response.message);
       } catch (e) {
         this.errorMessage = e.response.data;
-        this.component = "error-popup";
+        this.component = 'error-popup';
         setTimeout(() => {
           this.close();
         }, 3000);
@@ -568,13 +614,13 @@ export default {
       }
     },
     close() {
-      this.component = "";
+      this.component = '';
     },
     async submit() {
       this.uploadLoading = true;
       let formData = new FormData();
-      formData.append("files", this.files);
-      formData.append("type", "profileImage");
+      formData.append('files', this.files);
+      formData.append('type', 'profileImage');
       this.files = null;
 
       let fileResponse;
@@ -584,20 +630,20 @@ export default {
           formData,
           {
             headers: {
-              "Content-Type": "multipart/form-data",
-              user: this.userId
-            }
+              'Content-Type': 'multipart/form-data',
+              user: this.userId,
+            },
           }
         );
         this.uploadLoading = false;
-        this.component = "success-popup";
-        this.successMessage = "Profile successfully updated";
-        console.log("group people response", this.taskFiles);
+        this.component = 'success-popup';
+        this.successMessage = 'Profile successfully updated';
+        console.log('group people response', this.taskFiles);
         location.reload();
       } catch (e) {
-        console.log("Error uploading prof pic: ", e);
-        this.component = "error-popup";
-        console.log("File Upload Failed: " + e);
+        console.log('Error uploading prof pic: ', e);
+        this.component = 'error-popup';
+        console.log('File Upload Failed: ' + e);
         this.errorMessage = e.response.data;
         this.uploadLoading = false;
       }
@@ -609,18 +655,16 @@ export default {
       } catch (e) {
         console.log(e);
       }
-    }
+    },
   },
   validations: {
     password: {
       required,
-      minLength: minLength(6)
+      minLength: minLength(6),
     },
     confirmPassword: {
-      sameAsPassword: sameAs("password")
-    }
-  }
+      sameAsPassword: sameAs('password'),
+    },
+  },
 };
 </script>
-
-
