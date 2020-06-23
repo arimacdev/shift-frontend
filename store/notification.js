f2caeabb6cc7a91266b74d1cad2c3e6e363b5370
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-06-23 19:12:02 +0530

    updated

--- store/notification.js
+++ store/notification.js
@@ -5,11 +5,6 @@ export const mutations = {};
 export const actions = {
   async addNotificationDevice({ commit, rootState }, subscriptionId) {
     const user = rootState.user.userId;
-    // let formData = new FormData();
-    // formData.append('subscriptionId', subscriptionId);
-    // formData.append('subscriberId', user);
-    // formData.append('provider', 'OneSignal');
-    // formData.append('platform', 'Web');
     try {
       response = await this.$axios.$post(
         `/notification/register`,
