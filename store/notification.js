99bb85a729be4594bcee5cba920153d240f25400
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-06-23 16:18:51 +0530

    notification updated

--- store/notification.js
+++ store/notification.js
@@ -1,30 +1,30 @@
-export const state = () => ({
-});
+export const state = () => ({});
 
-export const mutations = {}
+export const mutations = {};
 
 export const actions = {
-    async addNotificationDevice({ commit, rootState }, subscriptionId) {
-        const user = rootState.user.userId;        
-          try {
-          response = await this.$axios.$post(
-            `/notification/register`,
-            {
-              headers: {
-                user: user,
-              },
-            },
-            {
-              data: {
-                subscriptionId: subscriptionId,
-                subscriberId: user,
-                provider: "OneSignal",
-                platform: "Web"
-              },
-            }
-          );
-        } catch (e) {
-        console.log("error", e)
-      }
-      }
-}
+  async addNotificationDevice({ commit, rootState }, subscriptionId) {
+    const user = rootState.user.userId;
+    try {
+      response = await this.$axios.$post(
+        `/notification/register`,
+        {
+          headers: {
+            userId: user,
+          },
+        },
+        {
+          data: {
+            subscriptionId: subscriptionId,
+            subscriberId: user,
+            provider: 'OneSignal',
+            platform: 'Web',
+          },
+        }
+      );
+      console.log('RESPONSE', response);
+    } catch (e) {
+      console.log('error', e);
+    }
+  },
+};
