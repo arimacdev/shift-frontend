7eb41d0cea724114eec07bce0e5ae54d489626b5
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-06-30 13:45:28 +0530

    updated

--- pages/comments/comments.vue
+++ pages/comments/comments.vue
@@ -1,3 +1,139 @@
 <template>
-  <h1></h1>
+  <div>
+    <div style="height: 110vh">zdzxzc</div>
+    <div class="control-section">
+      <div class="sample-container">
+        <div class="default-section">
+          <ejs-richtexteditor
+            ref="rteObj"
+            :quickToolbarSettings="quickToolbarSettings"
+            :height="340"
+            :toolbarSettings="toolbarSettings"
+          >
+            <p>
+              The Rich Text Editor is WYSIWYG ("what you see is what you get") editor useful to create and edit content,and return the valid
+              <a
+                href="https://ej2.syncfusion.com/home/"
+                target="_blank"
+              >HTML markup</a> or
+              <a href="https://ej2.syncfusion.com/home/" target="_blank">markdown</a>
+              of the content
+            </p>
+            <p>
+              <b>Key features:</b>
+            </p>
+            <ul>
+              <li>
+                <p>Provides IFRAME and DIV modes</p>
+              </li>
+              <li>
+                <p>Capable of handling markdown editing.</p>
+              </li>
+              <li>
+                <p>Contains a modular library to load the necessary functionality on demand.</p>
+              </li>
+              <li>
+                <p>Provides a fully customizable toolbar.</p>
+              </li>
+              <li>
+                <p>Provides HTML view to edit the source directly for developers.</p>
+              </li>
+              <li>
+                <p>Supports third-party library integration.</p>
+              </li>
+              <li>
+                <p>Allows preview of modified content before saving it.</p>
+              </li>
+              <li>
+                <p>Handles images, hyperlinks, video, hyperlinks, uploads, etc.</p>
+              </li>
+            </ul>
+          </ejs-richtexteditor>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+import Vue from "vue";
+import {
+  RichTextEditorPlugin,
+  Toolbar,
+  Image,
+  Link,
+  HtmlEditor,
+  QuickToolbar
+} from "@syncfusion/ej2-vue-richtexteditor";
+
+Vue.use(RichTextEditorPlugin);
+
+export default {
+  data: function() {
+    return {
+      height: 400,
+      quickToolbarSettings: {
+        image: [
+          "Replace",
+          "Align",
+          "Caption",
+          "Remove",
+          "InsertLink",
+          "OpenImageLink",
+          "-",
+          "EditImageLink",
+          "RemoveImageLink",
+          "Display",
+          "AltText",
+          "Dimension"
+        ],
+        link: ["Open", "Edit", "UnLink"]
+      },
+      toolbarSettings: {
+        items: [
+          "Bold",
+          "Italic",
+          "Underline",
+          "StrikeThrough",
+          "FontName",
+          "FontSize",
+          "FontColor",
+          "BackgroundColor",
+          "LowerCase",
+          "UpperCase",
+          "|",
+          "Formats",
+          "Alignments",
+          "OrderedList",
+          "UnorderedList",
+          "Outdent",
+          "Indent",
+          "|",
+          "CreateLink",
+          "Image",
+          "|",
+          "ClearFormat",
+          "Print",
+          "SourceCode",
+          "FullScreen",
+          "|",
+          "Undo",
+          "Redo"
+        ]
+      }
+    };
+  },
+  provide: {
+    richtexteditor: [Toolbar, Image, Link, HtmlEditor, QuickToolbar]
+  }
+};
+</script>
+<style>
+@import "../../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-lists/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-popups/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-vue-richtexteditor/styles/material.css";
+</style>
