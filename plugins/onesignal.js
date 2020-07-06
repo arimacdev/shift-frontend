9bfb6bfd3aff3ea7e0a64d6caa794962fff95e01
Naveen <naveenperera777@gmail.com>
2020-07-06 11:52:31 +0530

    OneSignal Request only when user exists

--- plugins/onesignal.js
+++ plugins/onesignal.js
@@ -6,7 +6,9 @@ OneSignal.push(() => {
   console.log('Hello, from Onesignal');
   OneSignal.getUserId().then((userId)=> {
     console.log('OneSignal User ID:', userId);
+    if(userId != null){
     context.store.dispatch('notification/addNotificationDevice', userId);
+    }
   });
   OneSignal.init({
     appId: 'fe6df906-c5cf-4c5e-bc1f-21003be4b2d5',
