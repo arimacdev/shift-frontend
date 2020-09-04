42d786de4b95899fe858159d8a08a443f79db969
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-09-04 16:34:57 +0530

    user restrictions

--- pages/admin/admin.vue
+++ pages/admin/admin.vue
@@ -1,48 +1,78 @@
 <template>
-  <div class="top-nav">
-    <navigation-drawer :user="user" />
+  <div>
+    <div
+      v-if="organizationalRoles.indexOf('SUPER_ADMIN') > -1 ||
+                  organizationalRoles.indexOf('WORKLOAD') > -1 ||
+                  organizationalRoles.indexOf('ADMIN') > -1"
+      class="top-nav"
+    >
+      <navigation-drawer :user="user" />
 
-    <v-toolbar app color dark fixed :clipped-left="clipped" class="toolBarFilter tool-bar">
-      <div class="title-div">
-        <div class="name-div">
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="font-weight-bold">Admin</v-list-item-title>
-            </v-list-item-content>
+      <v-toolbar app color dark fixed :clipped-left="clipped" class="toolBarFilter tool-bar">
+        <div class="title-div">
+          <div class="name-div">
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="font-weight-bold">Admin</v-list-item-title>
+              </v-list-item-content>
 
-            <v-divider class="mx-4" inset vertical></v-divider>
-          </v-list-item>
+              <v-divider class="mx-4" inset vertical></v-divider>
+            </v-list-item>
+          </div>
+        </div>
+      </v-toolbar>
+
+      <div class="body-div">
+        <div class="workloadTypeSection">
+          <v-tabs background-color="#151515" slider-size="3" dark>
+            <v-tab
+              class="tabInactiveStyle text-capitalize"
+              active-class="adminTabTitleStyle text-capitalize"
+              v-on:click="component = 'users-tab'"
+            >Users</v-tab>
+            <v-tab
+              class="tabInactiveStyle text-capitalize"
+              active-class="adminTabTitleStyle text-capitalize text-capitalize"
+              v-on:click="component = 'skills-tab'"
+            >Skills</v-tab>
+            <v-tab
+              class="tabInactiveStyle text-capitalize"
+              active-class="adminTabTitleStyle text-capitalize"
+              v-on:click="component = 'organization-tab'"
+            >Organization</v-tab>
+          </v-tabs>
         </div>
       </div>
-    </v-toolbar>
 
-    <div class="body-div">
-      <div class="workloadTypeSection">
-        <v-tabs background-color="#151515" slider-size="3" dark>
-          <v-tab
-            class="tabInactiveStyle text-capitalize"
-            active-class="adminTabTitleStyle text-capitalize"
-            v-on:click="component = 'users-tab'"
-          >Users</v-tab>
-          <v-tab
-            class="tabInactiveStyle text-capitalize"
-            active-class="adminTabTitleStyle text-capitalize text-capitalize"
-            v-on:click="component = 'skills-tab'"
-          >Skills</v-tab>
-          <v-tab
-            class="tabInactiveStyle text-capitalize"
-            active-class="adminTabTitleStyle text-capitalize"
-            v-on:click="component = 'organization-tab'"
-          >Organization</v-tab>
-        </v-tabs>
+      <div class="workloadV2Body">
+        <component v-bind:is="component"></component>
+      </div>
+      <v-overlay :value="overlay" color="black" style="z-index:1008">
+        <progress-loading />
+      </v-overlay>
+    </div>
+    <div v-else>
+      <div style="text-align: center">
+        <v-row class="errorPageContainer">
+          <v-col class="errorPageImage">
+            <img
+              align="center"
+              height="100%"
+              src="https://arimac-pmtool.s3-ap-southeast-1.amazonaws.com/projectFile_1592549257245_Asset%202@4x.png"
+              alt
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="errorPageText">
+            <div class="errorMessage">Not Found!</div>
+            <NuxtLink to="/" style="text-decoration: none">
+              <v-btn small color="red" dark>Back to home</v-btn>
+            </NuxtLink>
+          </v-col>
+        </v-row>
       </div>
     </div>
-    <div class="workloadV2Body">
-      <component v-bind:is="component"></component>
-    </div>
-    <v-overlay :value="overlay" color="black" style="z-index:1008">
-      <progress-loading />
-    </v-overlay>
   </div>
 </template>
 <script>
@@ -51,6 +81,8 @@ import Users from "~/components/admin/users";
 import Organization from "~/components/admin/organization";
 import Progress from "~/components/popups/progress";
 import Skills from "~/components/admin/skills";
+
+import { mapState, mapGetters } from "vuex";
 
 export default {
   components: {
@@ -65,6 +97,19 @@ export default {
       overlay: false,
       component: "users-tab",
     };
+  },
+  methods: {
+    aaaaa() {
+      console.log("ERROR");
+      // window.location.href = "/";
+      location.reload();
+    },
+  },
+  computed: {
+    ...mapState({
+      taskWorkLoadUsers: (state) => state.workload.taskWorkLoadUsers,
+      organizationalRoles: (state) => state.user.organizationalRoles,
+    }),
   },
   async asyncData({ $axios, store }) {
     let userId = store.state.user.userId;
