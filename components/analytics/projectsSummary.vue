843ce6a332420b337eee77c0c44443fb5c05968b
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2020-09-08 09:37:47 +0530

    leads display field UI design

--- components/analytics/projectsSummary.vue
+++ components/analytics/projectsSummary.vue
@@ -1,3 +1,79 @@
 <template>
-  <h1>Projects</h1>
+  <div class="projectSummarypage">
+    <div class="projectTopSection">
+      <div class="leadsDisplay">
+        <div class="leadsContentDiv" style="background-color: #66B25F">
+          <v-list-item>
+            <v-list-item-icon style="background-color: #FFFFFF; padding: 15px; border-radius: 50%">
+              <v-icon size="20">icon-project</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-subtitle class="leadsSubTitle">Total Projects</v-list-item-subtitle>
+              <v-list-item-title class="leadsTitle">125</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </div>
+        <div class="leadsAnalyticsText">2 (+1%) over the past 30 days</div>
+      </div>
+      <div class="leadsDisplay">
+        <div class="leadsContentDiv" style="background-color: #E07857">
+          <v-list-item>
+            <v-list-item-icon style="background-color: #FFFFFF; padding: 15px; border-radius: 50%">
+              <v-icon size="20">icon-project</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-subtitle class="leadsSubTitle">Pending Leads</v-list-item-subtitle>
+              <v-list-item-title class="leadsTitle">14</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </div>
+        <div class="leadsAnalyticsText">2 (+1%) over the past 30 days</div>
+      </div>
+      <div class="leadsDisplay">
+        <div class="leadsContentDiv" style="background-color: #67D2E0">
+          <v-list-item>
+            <v-list-item-icon style="background-color: #FFFFFF; padding: 15px; border-radius: 50%">
+              <v-icon size="20">icon-project</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-subtitle class="leadsSubTitle">Leads Ongoing</v-list-item-subtitle>
+              <v-list-item-title class="leadsTitle">44</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </div>
+        <div class="leadsAnalyticsText">2 (+1%) over the past 30 days</div>
+      </div>
+      <div class="leadsDisplay">
+        <div class="leadsContentDiv" style="background-color: #E0CA57">
+          <v-list-item>
+            <v-list-item-icon style="background-color: #FFFFFF; padding: 15px; border-radius: 50%">
+              <v-icon size="20">icon-project</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-subtitle class="leadsSubTitle">Leads Conversion</v-list-item-subtitle>
+              <v-list-item-title class="leadsTitle">153</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </div>
+        <div class="leadsAnalyticsText">2 (+1%) over the past 30 days</div>
+      </div>
+      <div class="leadsDisplay">
+        <div class="leadsContentDiv">
+          <div>
+            <v-text-field solo dense background-color="#EDF0F5" flat label="All time"></v-text-field>
+
+            <v-text-field
+              style="margin-top: -20px !important"
+              solo
+              dense
+              background-color="#EDF0F5"
+              flat
+              label="Feb 2020 - Mar 2020"
+            ></v-text-field>
+          </div>
+          <div>Feb 2020 - mar 2020</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
